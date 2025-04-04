1e34087b5476b34d97e18b9531be7cdd4c6ffd69
Ari LiVigni <arilivigni@github.com>
2025-04-04 12:58:51 +0000

    Add postStartCommand and create post_start.sh for port visibility setup

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -3,7 +3,3 @@
 # It is used to install any additional dependencies or perform any setup tasks.
 
 sudo cp --force ./.devcontainer/welcome-message.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
-
-# open ports for Python Django server and React app
-gh cs ports visibility 8000:public -c $CODESPACE_NAME
-gh cs ports visibility 3000:public -c $CODESPACE_NAME
