66fe63cecb6382eed7bb2604f0e708aa6bff9fd7
Ari LiVigni <arilivigni@github.com>
2025-08-10 15:41:10 +0000

    fix: ensure directory exists before copying welcome message in post_create.sh

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -7,4 +7,5 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb
 sudo apt-get update
 sudo apt-get install -y python3-venv
 sudo apt-get install -y mongodb-org
-  sudo cp --force ".devcontainer/welcome-message.txt" /usr/local/etc/vscode-dev-containers/first-run-notice.txt
+sudo mkdir -p /usr/local/etc/vscode-dev-containers
+sudo cp --force ./.devcontainer/welcome-message.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
